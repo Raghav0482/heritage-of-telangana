cf62c77520d07a94d80109634a6d8cecf8e44c96
Raghav <Raghav0482@gmail.com>
2021-01-11 15:29:52 +0530

    changes

--- js/script.js
+++ js/script.js
@@ -90,3 +90,21 @@ function ValidateEmail(inputText) {
     document.form1.text1.focus();
   }
 }
+
+mybutton = document.getElementById("myBtn")
+            // when the user scrolls down 20px  from top of the document, show the button
+            window.onscroll = function () { scrollFunction() }
+            function scrollFunction() {
+                  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+                        mybutton.style.display = "block";
+                  } else {
+                        mybutton.style.display = "none"
+                  }
+
+            }
+
+            // when user click on the button, scroll to the top of the document 
+            function myFunction() {
+                  document.body.scrollTop = 0; // for safari
+                  document.documentElement.scrollTop = 0;// for chrome, firefox,opera,internet explorer
+            }
